409d2481c98afd2caea5b8b692b66c24a0d2b3d7
maxime.cognie <maxime.cognie@protonmail.com>
2024-09-10 06:40:30 +0000

    Checkpoint 21. Web Development for Robotics

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # tortoisebot_webapp
+
+---
+
+This package implement a web application to interact with a tortoisebot.
+Using this web interface you will be able to control the robot using different control method and have feedback from the robot.
+
+Control:
+    - Manual control using a joystcik
+    - Send 'move to waypoint X' command
+
+Feedback:
+    - Front camera
+    - Map generated with SLAM algorythm
+    - 3D model of the robot
+
+
+## Instalation
+---
+
+### prerequisites
+
+- ROS
+- Gazebo
+- webbrowser         
+
+### Install
+
+ 1- Clone the repository inside your workspace:  
+`git clone https://github.com/maxime-cognie/tortoisebot_webapp.git`  
+
+
+## Test the web-app
+---
+
+To test the web-app:    
+- Launch the simulation:   
+'roslaunch tortoisebot_gazebo tortoisebot_docking.launch'
+
+- Launch ROSBridge + the video servers:      
+`roslaunch course_web_dev_ros web.launch' 
+
+- Launch the mapping node:
+'roslaunch tortoisebot_slam mapping.launch'
+
+- Launch the waypoints action server:
+'rosrun course_web_dev_ros tortoisebot_action_server.py'
+
+- Start the web-app:
+'''
+cd ~/webpage_ws/tortoisebot_webapp
+python -m http.server 7000
+'''
---
+++ main.js
@@ -0,0 +1,255 @@
+var app = new Vue({
+    el: '#app',
+    // storing the state of the page
+    data: {
+        connected: false,
+        ros: null,
+        logs: [],
+        loading: false,
+        rosbridge_address: 'wss://i-0e5e1254f8a2a6e3b.robotigniteacademy.com/5ba9fdc4-e000-4d0f-a481-00dc1232b573/rosbridge/',
+        port: '9090',
+        interval: null,
+        // Mapping stuff
+        mapViewer: null,
+        mapGridClient: null,
+        // 3D stuff
+        viewer: null,
+        tfClient: null,
+        urdfClient: null,
+        // dragging data
+        dragging: false,
+        x: 'no',
+        y: 'no',
+        dragCircleStyle: {
+            margin: '0px',
+            top: '0px',
+            left: '0px',
+            display: 'none',
+            width: '75px',
+            height: '75px',
+        },
+        // joystick valules
+        joystick: {
+            vertical: 0,
+            horizontal: 0,
+        },
+        // Action messages
+        goal: null,
+        action: {
+            goal: { position: {x: 0, y: 0, z: 0} },
+            feedback: { position: 0, state: 'idle' },
+            result: { success: false },
+            status: { status: 0, text: '' },
+        }
+    },
+    // helper methods to connect to ROS
+    methods: {
+        connect: function() {
+            this.loading = true
+            this.ros = new ROSLIB.Ros({
+                url: this.rosbridge_address
+            })
+            this.ros.on('connection', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
+                this.connected = true
+                this.loading = false
+                this.setCamera()
+                this.setup3DViewer()
+                this.setupMapViewer()
+            })
+            this.ros.on('error', (error) => {
+                this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
+            })
+            this.ros.on('close', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
+                this.connected = false
+                this.loading = false
+                this.unsetCamera()
+                this.unset3DViewer()
+                this.unsetMap()
+            })
+        },
+        disconnect: function() {
+            this.ros.close()
+        },
+        clearLog: function() {
+            this.logs = []
+        },
+        setCamera: function() {
+            let without_wss = this.rosbridge_address.split('wss://')[1]
+            console.log(without_wss)
+            let domain = without_wss.split('/')[0] + '/' + without_wss.split('/')[1]
+            console.log(domain)
+            let host = domain + '/cameras'
+            let viewer = new MJPEGCANVAS.Viewer({
+                divID: 'divCamera',
+                host: host,
+                width: 400,
+                height: 225,
+                topic: 'camera/image_raw',
+                ssl: true,
+            })
+        },
+        unsetCamera: function() {
+            document.getElementById('divCamera').innerHTML = ''
+        },
+        setupMapViewer: function() {
+            this.mapViewer = new ROS2D.Viewer({
+                divID: 'divMap',
+                width: 1040,
+                height: 585
+            })
+
+            // Setup the map client.
+            this.mapGridClient = new ROS2D.OccupancyGridClient({
+                ros: this.ros,
+                rootObject: this.mapViewer.scene,
+                continuous: true,
+            })
+            // Scale the canvas to fit to the map
+            this.mapGridClient.on('change', () => {
+                this.mapViewer.scaleToDimensions(this.mapGridClient.currentGrid.width, this.mapGridClient.currentGrid.height);
+                this.mapViewer.shift(this.mapGridClient.currentGrid.pose.position.x, this.mapGridClient.currentGrid.pose.position.y)
+            })
+        },
+        unsetMap: function() {
+            document.getElementById('divMap').innerHTML = ''
+        },
+        setup3DViewer: function() {
+            this.viewer = new ROS3D.Viewer({
+                background: '#cccccc',
+                divID: 'div3DViewer',
+                width: 416,
+                height: 234,
+                antialias: true,
+                fixedFrame: 'odom'
+            })
+
+            // Add a grid.
+            this.viewer.addObject(new ROS3D.Grid({
+                color:'#0181c4',
+                cellSize: 0.5,
+                num_cells: 100
+            }))
+
+            // Setup a client to listen to TFs.
+            this.tfClient = new ROSLIB.TFClient({
+                ros: this.ros,
+                angularThres: 0.01,
+                transThres: 0.01,
+                rate: 10.0
+            })
+
+            // Setup the URDF client.
+            this.urdfClient = new ROS3D.UrdfClient({
+                ros: this.ros,
+                param: 'robot_description',
+                tfClient: this.tfClient,
+                // We use "path: location.origin + location.pathname"
+                // instead of "path: window.location.href" to remove query params,
+                // otherwise the assets fail to load
+                path: "./tortoisebot_description/urdf", //location.origin + location.pathname,
+                rootObject: this.viewer.scene,
+                loader: ROS3D.COLLADA_LOADER_2
+            })
+        },
+        unset3DViewer() {
+            document.getElementById('div3DViewer').innerHTML = ''
+        },
+        sendCommand: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: this.joystick.vertical , y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: this.joystick.horizontal, },
+            })
+            topic.publish(message)
+        },
+        startDrag() {
+            this.dragging = true
+            this.x = this.y = 0
+        },
+        stopDrag() {
+            this.dragging = false
+            this.x = this.y = 'no'
+            this.dragCircleStyle.display = 'none'
+            this.resetJoystickVals()
+        },
+        doDrag(event) {
+            if (this.dragging) {
+                this.x = event.offsetX
+                this.y = event.offsetY
+                let ref = document.getElementById('dragstartzone')
+                this.dragCircleStyle.display = 'inline-block'
+
+                let minTop = ref.offsetTop - parseInt(this.dragCircleStyle.height) / 2
+                let maxTop = minTop + 200
+                let top = this.y + minTop
+                this.dragCircleStyle.top = `${top}px`
+
+                let minLeft = ref.offsetLeft - parseInt(this.dragCircleStyle.width) / 2
+                let maxLeft = minLeft + 200
+                let left = this.x + minLeft
+                this.dragCircleStyle.left = `${left}px`
+
+                this.setJoystickVals()
+                this.sendCommand()
+            }
+        },
+        setJoystickVals() {
+            this.joystick.vertical = -1 * ((this.y / 200) - 0.5)
+            this.joystick.horizontal = +1 * ((this.x / 200) - 0.5)
+        },
+        resetJoystickVals() {
+            this.joystick.vertical = 0
+            this.joystick.horizontal = 0
+        },
+        sendGoal: function() {
+            let actionClient = new ROSLIB.ActionClient({
+                ros : this.ros,
+                serverName : '/tortoisebot_as',
+                actionName : 'course_web_dev_ros/WaypointActionAction'
+            })
+
+            this.goal = new ROSLIB.Goal({
+                actionClient : actionClient,
+                goalMessage: {
+                    ...this.action.goal
+                }
+            })
+
+            this.goal.on('status', (status) => {
+                this.action.status = status
+            })
+
+            this.goal.on('feedback', (feedback) => {
+                this.action.feedback = feedback
+            })
+
+            this.goal.on('result', (result) => {
+                this.action.result = result
+            })
+
+            this.goal.send()
+        },
+        setGoal: function(x, y) {
+            this.action.goal.position.x = x;
+            this.action.goal.position.y = y;
+        },
+        cancelGoal: function() {
+            this.goal.cancel()
+        },
+    },
+    mounted() {
+        // page is ready
+        window.addEventListener('mouseup', this.stopDrag)
+        this.interval = setInterval(() => {
+            if (this.ros != null && this.ros.isConnected) {
+                this.ros.getNodes((data) => { }, (error) => { })
+            }
+        }, 10000)
+    },
+})
